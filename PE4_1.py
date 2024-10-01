7d4ba89cc1fc136efcc770db8540654e4979cafa
Vladus323 <VLADISLAV.LOPUKHOV76@student.qcc.cuny.edu>
2024-10-01 13:27:35 -0400

    Add files via upload

---
+++ PE4_1.py
@@ -0,0 +1,17 @@
+print("Python")
+print("Python"[0])
+print("Python"[-1])
+print("Python"[:])
+
+
+str = "Python 123"
+print(str)
+print(str[0])
+print(str[-1])
+print(str[:])
+
+
+strNum = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(strNum[1], strNum[-1], len(strNum))
+print(strNum[:len(strNum)])
+print(strNum[1]+strNum[-3])
