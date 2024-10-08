f7c2cc962757d80abc417e670b0e7a3b8397be53
Vladus323 <VLADISLAV.LOPUKHOV76@student.qcc.cuny.edu>
2024-10-07 21:11:20 -0400

    Add files via upload

---
+++ PE4_3.py
@@ -0,0 +1,21 @@
+
+courses = ["ET574", "ET704", "MA114", "ENGL101"]  
+
+
+print("List of courses:", courses)
+
+
+print(f"I am taking {len(courses)} courses")
+
+
+print("First course:", courses[0])
+print("Last course:", courses[-1])
+
+
+print("First four courses:", courses[:4])
+
+
+print("Last four courses:", courses[-4:])
+
+
+print("Courses except first and last:", courses[1:-1])
